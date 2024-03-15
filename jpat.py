14f4f8699fff02051d3aee42df1e4fb1f64bf0a0
GitHubに登録したユーザ名/Users/tei/opt/anaconda3/envs/aidemy_python_3-9-18/bin/python <t.tsukashi@gmail.com>
2024-03-15 22:24:23 +0900

    変更内容

--- jpat.py
+++ jpat.py
@@ -3,7 +3,6 @@ from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.optimizers import SGD  # SGDをインポートする
 
 import numpy as np
 
@@ -18,15 +17,7 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-###model = load_model('./model.h5') #学習済みモデルをロード
-
-# SGDオプティマイザを再定義
-class CustomSGD(SGD):
-    pass
-
-# モデルをロードする際にカスタムオブジェクトとして指定
-model = load_model('./model.h5', custom_objects={'CustomSGD': CustomSGD})
-
+model = load_model('./model.h5') #学習済みモデルをロード
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
